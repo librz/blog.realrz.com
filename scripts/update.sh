e7b47f932f654ea26c2cafe9d4a9c2555ce948c4
rz <rz19960718@gmail.com>
2021-02-01 20:38:28 +0800

    chore: small improvements

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+function err {
+	echo "$1" >&2
+}
+
 # check whether there's a process running on port 9001
 # if there is, kill it
 
-result=$(bash <(curl -sL https://raw.githubusercontent.com/librz/shell_scripts/main/3p.sh) --port 9001)
-
-if [[ "$?" -eq 0 ]]; then
+if result=$(
+	bash <(curl -sL https://raw.githubusercontent.com/librz/shell_scripts/main/3p.sh) --port 9001
+); then
     # get pid & kill it
     pid=$(echo "$result" | awk 'NR>1{print $2}')
 		echo "found process with pid $pid already running on port 9001"
 		echo "killing it..."
     if kill "$pid"; then
-			echo "killing success"
+			echo "killed process with pid $pid"
 		else
-			echo "killing process $pid failed"
+			err "failed to kill process with pid $pid"
 			exit 1
 		fi
 fi
