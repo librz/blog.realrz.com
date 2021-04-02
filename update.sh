3562a2dd20624e264d2a8b92b6692479404dce9c
rz <rz19960718@gmail.com>
2021-04-03 00:29:52 +0800

    curl remote script gsync.sh

--- update.sh
+++ update.sh
@@ -12,28 +12,9 @@ fi
 
 echo "----"
 echo "check to see if local repo is up to date"
-if ! git fetch origin; then
-	err "fail to run git fetch, check your network connectivity"
+if ! bash <(curl -sL http:realrz.com/shell-scripts/gsync.sh); then
 	exit 1
 fi
-
-if [[ -z $(git diff origin/main) ]]; then
-	echo "you already have the latest code"	
-	echo "this may happen when you used 'git pull' or 'git merge' mannually"
-	echo "or simply there's nothing new on the remote repo"
-	read -r -p "still want to build & deploy anyway? (Y/N) " answer
-	if [[ "$answer" =~ ^[nN][oO]? ]]; then
-	   exit
-	fi	   
-else
-	echo "merging with origin/main"
-	if ! git merge origin/main; then
-		err "merge failed"
-		exit 2
-	fi
-fi
-
-echo
 
 # check whether there's a process running on port 9001
 # if there is, kill it
