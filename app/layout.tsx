fc21fabb19508c42759c2b984007990accaf7386
librz <rz19960718@gmail.com>
2023-04-24 23:44:18 +0800

    add github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import GithubLogo from "assets/github.svg";
+import Image from "next/image";
 import "lxgw-wenkai-webfont/style.css";
 import "./global.css";
 
@@ -15,12 +17,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header className="h-14 px-8 w-full bg-neutral-900 shadow flex items-center text-gray-300">
+        <header className="h-14 px-8 w-full bg-neutral-900 shadow flex items-center justify-between text-gray-300">
           <Link href={"/"}>Home</Link>
+          <Link
+            href={"https://github.com/librz/blog.realrz.com"}
+            target="_blank"
+            style={{ textDecoration: "underline" }}
+          >
+            <Image
+              priority
+              src={GithubLogo}
+              alt="Github"
+              width={24}
+            />
+          </Link>
         </header>
         <main className="min-h-screen flex flex-col items-center pt-4 pb-8 px-6">{children}</main>
         <footer className="h-14 w-full bg-neutral-900 shadow flex items-center justify-center text-gray-300">
-          Copyright &#169; {new Date().getFullYear()} realrz
+          <Link href={"https://www.buymeacoffee.com/realrz"} target="_blank" style={{ textDecoration: "underline" }}>
+            Buy me a coffee
+          </Link>
         </footer>
       </body>
     </html>
