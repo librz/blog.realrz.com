fea295c72b301b785a2ec496336503ca61f85fb6
librz <rz19960718@gmail.com>
2023-05-02 12:26:56 +0800

    fix serve doesn't work issue

--- app/[blog]/page.tsx
+++ app/[blog]/page.tsx
@@ -14,7 +14,7 @@ export default async function BlogPost({
   }
 
   return (
-    <article className='prose max-w-full md:max-w-prose lg:prose-lg prose-img:rounded-lg prose-pre:py-1 prose-pre:px-2 my-4'>
+    <article className='prose max-w-full md:max-w-prose lg:prose-lg prose-img:rounded-lg my-4'>
       <h2 style={{ textAlign: "center", color: "royalblue" }}>{post.title}</h2>
       <MDX code={post.body.code} />
     </article>
--- components/Test.tsx
+++
@@ -1,5 +0,0 @@
-export default ({ text }: { text: string }) => {
-  return (
-    <div style={{ fontSize: 20, color: "orange" }}>{text.repeat(10)}</div>
-  )
-}
