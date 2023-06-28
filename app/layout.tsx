f5df1bb4d25be4ea5926ba278cb25d8943e86e2c
librz <rz19960718@gmail.com>
2023-06-28 11:07:46 +0800

    rm lxgw-wenkai-webfont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
-import "lxgw-wenkai-webfont/style.css";
 import "./global.css";
 
 export const metadata: Metadata = {
