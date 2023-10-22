f5cc095978daeef66b9410c27092e0841bd78a5b
librz <rz19960718@gmail.com>
2023-10-23 03:02:27 +0800

    upgrade next.js to 13.5.6S

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-
 /** @type {import('next').NextConfig} */
-const { withContentlayer } = require('next-contentlayer');
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 module.exports = withContentlayer({
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
 });
